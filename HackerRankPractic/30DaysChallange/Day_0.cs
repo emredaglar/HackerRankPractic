a7b79ff3638f370fcf0a677fe7398be30b434cde
emredaglar <emre.daglar1@gmail.com>
2024-11-06 22:38:54 +0300

    Add project files.

---
+++ HackerRankPractic/30DaysChallange/Day_0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPractic._30DaysChallange
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("HW");
+
+            // TODO: Write a line of code here that prints the contents of input_string to stdout.
+            Console.WriteLine(inputString);
+        }
+    }
+}
+
